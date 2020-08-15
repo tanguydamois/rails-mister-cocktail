f37939c77673683307f97ccdc2ae812000e0e4de
tanguydamois <tdamois@gmail.com>
2020-08-15 13:33:01 -0400

    validations missing has_many ingredients in cocktails and is unique  for a given... in dose

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,6 @@
 class Cocktail < ApplicationRecord
+  has_many :ingredient
+  has_many :doses, :dependent => :destroy
+
+  validates :name, uniqueness: true, presence: true
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, presence: true, uniqueness: true
+  validates :ingredient, presence: true, uniqueness: true
 end
