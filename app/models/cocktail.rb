ff6cb1116837d25d7d5155457ac762298c2cb27f
tanguydamois <tdamois@gmail.com>
2020-08-15 13:39:05 -0400

    seed

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
-  has_many :ingredient
   has_many :doses, :dependent => :destroy
-
+  has_many :ingredients
   validates :name, uniqueness: true, presence: true
+
+
 end
